b60dfbf63fd83b141a7b0ba19ff02f622574ce9b
aubreymoore <aubreymoore@guam.net>
2021-09-06 05:22:22 +0000

    Nikola auto commit.
    Nikola version: 8.0.2

--- git_authenticate.sh
+++ git_authenticate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Remember to add this file to .gitignore
 git config user.name aubreymoore
-git config user.password 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
+git config user.password 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
